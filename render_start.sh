cc6772fe3d0636a58588010e9bb3eb75c2b0080c
Snajdy92 <t.snajdar@pobox.sk>
2026-02-19 00:16:59 +0100

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,1 +1,7 @@
-
+#!/bin/sh
+echo "Starting Database Init..."
+python run.py init-db
+echo "Creating Roles..."
+python run.py create-roles
+echo "Starting Server..."
+python -m gunicorn --bind 0.0.0.0:10000 wsgi:app
